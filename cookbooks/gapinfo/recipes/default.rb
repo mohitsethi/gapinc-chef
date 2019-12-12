204c7eb9895c955d97f9236efa94c8eb3bb34a97
mohitsethi <mohit@sethis.in>
2019-12-12 10:15:44 +0530

    updated stuff

--- cookbooks/gapinfo/recipes/default.rb
+++ cookbooks/gapinfo/recipes/default.rb
@@ -9,27 +9,31 @@
 # node['gapinfo']['version_vim'] = 1.1
 
 
-int func1(value) do
+# int func1(value) do
+# end
+
+package 'apache2' do
+    action  :install
+    # version node['gapinfo']['version_vim']
+    # run-1: vim v1
+    # run-2: vim v2
 end
 
-if platform?('redhat')
-    package 'vim' do
-        action  :upgrade
-        version func1(node['gapinfo']['version_vim'])
-
-        # run-1: vim v1
-        # run-2: vim v2
-    end
-
-
-    file '/etc/chef/client.rb' do
-        action :create_if_missing
-        # action :create
-    end
-
-    user 'mohit' do
-        action :create
-    end
-else
-    # all other operating system
+cookbook_file '/var/www/index.html' do
+    source 'index.html'
+    mode '0644'
+    action :create
 end
+
+
+#    ddddd file '/etc/chef/client.rb' do
+#         action :create_if_missing
+#         # action :create
+#     end
+
+#     user 'mohit' do
+#         action :create
+#     end
+# else
+#     # all other operating system
+# end
